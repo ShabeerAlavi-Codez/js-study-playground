d63e4d48a497d7417409096f5a7c7942c5e792dc
abu <abumdnr@gmail.com>
2023-10-24 00:04:53 +0530

    added gitignore and string related files

---
+++ string.js
@@ -0,0 +1,46 @@
+//there are many ways to create strings 
+/*
+example
+const name = 'hitesh'
+const repoCount = 50
+
+console.log(name + repoCount +'values');
+
+//  the best way is as follows: using back ticks.(string interpolation)
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+this convenient way.
+
+string methods.
+
+
+*/
+
+// const name = 'hitesh'
+// const repoCount = 50
+// //one way
+// console.log(name + repoCount +'values');
+// //another way
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gameName = new String('GAMEOF THROWNS')
+// console.log(gameName[0]);
+// console.log(gameName[4]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(7));
+// console.log(gameName.indexOf('R'));
+
+// const newString = gameName.substring(5,18)
+// console.log(newString);
+// const lastString = gameName.slice(-1,1);
+// console.log(lastString);
+
+
+// const newPassword = " 2342424 2424 242342   "
+// console.log(newPassword)
+// console.log(newPassword.substring(4,7))
+// console.log(newPassword.trim());
+
+
